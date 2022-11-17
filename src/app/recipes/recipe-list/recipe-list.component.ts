2774c038166c48d0037ccd42409192a46418389f
yagamiar15 <dario11elme@hotmail.com>
2022-11-16 23:21:29 -0300

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Una receta de prueba', 'Es una receta de prueba', 'https://www.clarin.com/img/2021/05/04/cAHcrweF1_1256x620__1.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
