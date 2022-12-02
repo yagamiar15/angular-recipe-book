048b68a4514aa7e6a358c37bce1e1f848096a44d
yagamiar15 <dario11elme@hotmail.com>
2022-12-02 00:16:58 -0300

    agregados modelos, agregada logica basica de componentes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Una receta de prueba', 'Es una receta de prueba', 'https://www.clarin.com/img/2021/05/04/cAHcrweF1_1256x620__1.jpg')
+    new Recipe('Una receta de prueba', 'Es una receta de prueba', 'https://www.clarin.com/img/2021/05/04/cAHcrweF1_1256x620__1.jpg'),
+    new Recipe('Una receta mas', 'Es una receta mas para probar', 'https://www.clarin.com/img/2021/05/04/cAHcrweF1_1256x620__1.jpg')
   ];
 
   constructor() { }
